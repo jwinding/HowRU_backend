498c5bb4b9fb6d4193bfda1c07b9a1fdb997367b
Jacob Winding <jacobkwinding@gmail.com>
2020-03-30 11:07:40 +0200

    Added JPA dependency, and added a first draft of a User class

--- src/main/java/com/academy/HowRU/HowRuApplication.java
+++ src/main/java/com/academy/HowRU/HowRuApplication.java
@@ -10,4 +10,5 @@ public class HowRuApplication {
 		SpringApplication.run(HowRuApplication.class, args);
 	}
 
+
 }
---
+++ src/main/java/com/academy/HowRU/user/User.java
@@ -0,0 +1,86 @@
+package com.academy.HowRU.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Entity
+@Table(name = "HowRU_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private final String username;
+    private String password;
+    private String email;
+    private LocalDateTime createdAt;
+
+
+    public User(String username,String password, String email){
+        this.username=username;
+        this.email=email;
+        this.password=password;
+    }
+
+    public User(){
+        username="";
+        email="";
+        password="";
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
