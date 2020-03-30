ac01e1571fbe182e30d59dc53878c2fb8af04ac9
Jacob Winding <jacobkwinding@gmail.com>
2020-03-30 10:19:20 +0200

    initial commit

---
+++ src/main/java/com/academy/HowRU/HowRuApplication.java
@@ -0,0 +1,13 @@
+package com.academy.HowRU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowRuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowRuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/HowRU/HowRuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.HowRU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowRuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
