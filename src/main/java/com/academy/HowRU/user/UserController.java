521c1a3199eade2d865cc43e6ac7cdd031b150a6
Jacob Winding <jacobkwinding@gmail.com>
2020-04-02 11:32:33 +0200

    Added annotation to UserController

--- src/main/java/com/academy/HowRU/user/UserController.java
+++ src/main/java/com/academy/HowRU/user/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.time.LocalDateTime;
@@ -18,13 +15,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
-    UserViewService userViewService;
+    private UserViewService userViewService;
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @GetMapping("/user")
     public ResponseEntity<List<User>> getUserList(){
@@ -57,6 +55,7 @@ public class UserController {
         userService.registerNewNewUser(username,password,email);
 
     }
+
 
 
 }
