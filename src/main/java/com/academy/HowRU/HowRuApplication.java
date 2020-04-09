494ba0d4d074b240645b03edf23b8b9bf5411e93
Jacob Winding <jacobkwinding@gmail.com>
2020-04-09 12:41:36 +0200

    Fixed timezone to Sweden

--- src/main/java/com/academy/HowRU/HowRuApplication.java
+++ src/main/java/com/academy/HowRU/HowRuApplication.java
@@ -3,10 +3,13 @@ package com.academy.HowRU;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class HowRuApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Stockholm"));
 		SpringApplication.run(HowRuApplication.class, args);
 	}
 
